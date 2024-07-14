c0160171a0572090bc046a4e089984e0bc0854c7
Mohammed <fejan033@gmail.com>
2024-07-14 13:35:27 +0530

    Revise_Basic_html_csss

---
+++ reviseall/src/components/html_basics.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './css_basic.css';
+
+function Portf() {
+    return (
+        <div className="intro">
+            <div className="items"> Name : Mohammed Fejan</div>
+            <div className="items"> Desigantion : Senior Engineer</div>
+        </div>
+    );
+}
+
+export default Portf;
